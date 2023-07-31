44cebe44314dfd815e5a6a6c513415c192019cf4
DeFiDev <defidev2021@gmail.com>
2023-07-31 23:10:11 +0800

    update

---
+++ precache-manifest.6e57700bfb16d61d7d89c394a13de8d5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f3be58a4d9559f88d07",
+    "url": "/static/css/main.e6b1740a.chunk.css"
+  },
+  {
+    "revision": "6f3be58a4d9559f88d07",
+    "url": "/static/js/main.6f3be58a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "cc2486f2b7523bf23645",
+    "url": "/static/css/2.32cbabf3.chunk.css"
+  },
+  {
+    "revision": "cc2486f2b7523bf23645",
+    "url": "/static/js/2.cc2486f2.chunk.js"
+  },
+  {
+    "revision": "dc648e27ae128a48630b5f51e87e6bbd",
+    "url": "/static/media/textLogo.dc648e27.png"
+  },
+  {
+    "revision": "3c0eb968a09e916b49b5450467c7119d",
+    "url": "/static/media/bscscan.3c0eb968.svg"
+  },
+  {
+    "revision": "db92def1500c188c3dc7d5c703217b9c",
+    "url": "/static/media/totalMinerWrap.db92def1.png"
+  },
+  {
+    "revision": "091d3bd2e4f08f69a14b6088cadc219e",
+    "url": "/static/media/walletBalanceWrap.091d3bd2.png"
+  },
+  {
+    "revision": "1cc9265ad3c1dbad2f52426a60325dbd",
+    "url": "/static/media/fundWrap.1cc9265a.png"
+  },
+  {
+    "revision": "21dad5b1190aba873318dba9e8857829",
+    "url": "/static/media/dividendsWrap.21dad5b1.png"
+  },
+  {
+    "revision": "743a68a2fb865c901b0edbdea5a0ada3",
+    "url": "/static/media/dividendsWrapMobile.743a68a2.png"
+  },
+  {
+    "revision": "b53147133e3bc1272596ac2966778b81",
+    "url": "/static/media/stakeUnstakeWrap.b5314713.png"
+  },
+  {
+    "revision": "3df249b0adaab75906532f5e7d2734d9",
+    "url": "/static/media/claimWrap.3df249b0.png"
+  },
+  {
+    "revision": "5bdfc9c0cc673db962959ffc425d0291",
+    "url": "/static/media/mobileBg.5bdfc9c0.jpg"
+  },
+  {
+    "revision": "8eb4bd033118435091ed7265d545ef6b",
+    "url": "/static/media/bg.8eb4bd03.jpg"
+  },
+  {
+    "revision": "13c12cdff114793b5eb70176abd5c443",
+    "url": "/index.html"
+  }
+];
